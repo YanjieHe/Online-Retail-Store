c3bd5ebe4dfb5dc629b371412df864e4644cf4d7
Yanjie He <heyanjie0@outlook.com>
2019-03-30 22:05:56 -0400

    customer login

--- OnlineRetailStore/src/main/java/com/retail/store/controllers/CustomerController.java
+++ OnlineRetailStore/src/main/java/com/retail/store/controllers/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,5 +22,12 @@ public class CustomerController {
     public ResponseEntity<Object> getAllCustomers(int customerId) {
         Customer customer = customerService.fetchCustomerById(customerId);
         return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/customer_login", method = RequestMethod.POST)
+    public ResponseEntity<Object> customerLogin(
+            @RequestParam(value = "username", required = true) String username,
+            @RequestParam(value = "password", required = true) String password) {
+
     }
 }
---
+++ online-retail-store-web-app/src/Login.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import Header from "./Header";
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleSubmit(event) {
+        alert("username: " + this.state.username + ", password: " + this.state.password);
+        event.preventDefault();
+    }
+
+    bannerArea() {
+        return <section className="blog-banner-area" id="category">
+            <div className="container h-100">
+                <div className="blog-banner">
+                    <div className="text-center">
+                        <h1>Login / Register</h1>
+                        <nav aria-label="breadcrumb" className="banner-breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                                <li className="breadcrumb-item active" aria-current="page">Login/Register</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    }
+
+    loginBox() {
+        return <section className="login_box_area section-margin">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="login_box_img">
+                            <div className="hover">
+                                <h4>New to our website?</h4>
+                                <p>There are advances being made in science and technology everyday, and a good example
+                                    of this is the</p>
+                                <a className="button button-account" href="register.html">Create an Account</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="login_form_inner">
+                            <h3>Log in to enter</h3>
+                            <form className="row login_form" action="#/" id="contactForm" onSubmit={this.handleSubmit}>
+                                <div className="col-md-12 form-group">
+                                    <input type="text" className="form-control" id="name" name="username"
+                                           placeholder="Username" onFocus="this.placeholder = ''"
+                                           value={this.state.username}
+                                           onChange={this.handleChange}
+                                           onBlur="this.placeholder = 'Username'"/>
+                                </div>
+                                <div className="col-md-12 form-group">
+                                    <input type="text" className="form-control" id="name" name="password"
+                                           placeholder="Password" onFocus="this.placeholder = ''"
+                                           value={this.state.password}
+                                           onChange={this.handleChange}
+                                           onBlur="this.placeholder = 'Password'"/>
+                                </div>
+                                <div className="col-md-12 form-group">
+                                    <div className="creat_account">
+                                        <input type="checkbox" id="f-option2" name="selector"/>
+                                        <label htmlFor="f-option2">Keep me logged in</label>
+                                    </div>
+                                </div>
+                                <div className="col-md-12 form-group">
+                                    <button type="submit" value="submit" className="button button-login w-100">Log In
+                                    </button>
+                                    <a href="#">Forgot Password?</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                {this.bannerArea()}
+                {this.loginBox()}
+            </div>
+        );
+    }
+}
+
+export default Login;
