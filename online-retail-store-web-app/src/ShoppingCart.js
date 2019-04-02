57c7487c05f197ca96523956530ac866f784680d
Yanjie He <heyanjie0@outlook.com>
2019-04-01 22:54:07 -0400

    shopping cart

--- online-retail-store-web-app/src/ShoppingCart.js
+++ online-retail-store-web-app/src/ShoppingCart.js
@@ -24,7 +24,7 @@ class ShoppingCart extends React.Component {
     //     this.componentWillMount();
     // }
 
-    componentWillMount() {
+    componentDidMount() {
         fetch('http://localhost:8080/customer_information/' + this.state.sessionId, {
             method: 'GET',
             headers: {
